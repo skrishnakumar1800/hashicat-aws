bbdcf0324a4371ab92604432129518e81b5fa478
Krishna <bkrishna1800@gmail.com>
2021-02-10 18:59:19 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/bkrishna1800/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
-  acl    = "private"
   bucket_prefix = "bkri"
 }
